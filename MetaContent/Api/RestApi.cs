fa75e185d66b3b1053d8e447cf10f0c0d839efc6
RichMercer <rich@richmercer.com>
2016-07-12 15:50:15 +0100

    Moved RestResponse class to keep Api folder clean.

--- MetaContent/Api/RestApi.cs
+++ MetaContent/Api/RestApi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Xml.Serialization;
 using Telligent.Evolution.Extensibility.Rest.Version2;
 using HttpMethod = Telligent.Evolution.Extensibility.Rest.Version2.HttpMethod;
 
@@ -141,5 +142,13 @@ namespace ContentMetadata.Api
 
             #endregion
         }
+    }
+
+    [XmlRoot(ElementName = "Response")]
+    public class RestResponse : IRestResponse
+    {
+        public object Data { get; set; }
+        public string[] Errors { get; set; }
+        public string Name { get; set; }
     }
 }
